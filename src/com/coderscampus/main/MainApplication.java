b321c9ee5a9eae448c47b42428c1d4a422e8d4cd
amamolo9986 <amamolo9986@yahoo.com>
2023-02-05 16:59:04 -0600

    fourth commit

--- src/com/coderscampus/main/MainApplication.java
+++ src/com/coderscampus/main/MainApplication.java
@@ -1,74 +1,14 @@
 package com.coderscampus.main;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.Comparator;
 
 public class MainApplication {
 
 	public static void main(String[] args) throws IOException {
 		FileService fileService = new FileService();
 		StudentInfo[] studentArray = fileService.readStudents("studentmasterlist.csv");
-
-			try (BufferedWriter outputWriterCompsci = new BufferedWriter(new FileWriter("Course1.csv"));
-					BufferedWriter outputWriterApmth = new BufferedWriter(new FileWriter("Course2.csv"));
-					BufferedWriter outputWriterStat = new BufferedWriter(new FileWriter("Course3.csv"))) {
-		
-			for (int i = 0; i < studentArray.length; i++) {
-			}
-			Arrays.sort(studentArray, new Comparator<StudentInfo>() {
-
-				@Override
-				public int compare(StudentInfo person2, StudentInfo person1) {
-					return Integer.compare(person1.getGrade(), person2.getGrade());
-				}
-			});
-			for (StudentInfo info : studentArray) {
-				System.out.println(info);
-			}
-
-			StudentInfo[] compsciArray = new StudentInfo[100];
-			StudentInfo[] apmthArray = new StudentInfo[100];
-			StudentInfo[] statArray = new StudentInfo[100];
-
-			int csCtr = 0;
-			int apCtr = 0;
-			int statCtr = 0;
-
-			for (StudentInfo course : studentArray) {
-				if (course.getCourse().contains("COMPSCI")) {
-					compsciArray[csCtr++] = course;
-				}
-				if (course.getCourse().contains("APMTH")) {
-					apmthArray[apCtr++] = course;
-				}
-				if (course.getCourse().contains("STAT")) {
-					statArray[statCtr++] = course;
-				}
-			}
-
-			for (int i = 0; i < compsciArray.length; i++) {
-				outputWriterCompsci.write(compsciArray[i] + "");
-				outputWriterCompsci.newLine();
-			}
-			outputWriterCompsci.flush();
-			outputWriterCompsci.close();
-			
-			for (int i = 0; i < apmthArray.length; i++) {
-				outputWriterApmth.write(apmthArray[i] + "");
-				outputWriterApmth.newLine();
-			}
-			outputWriterApmth.flush();
-			outputWriterApmth.close();
-			
-			for (int i = 0; i < statArray.length; i++) {
-				outputWriterStat.write(statArray[i] + "");
-				outputWriterStat.newLine();
-			}
-			outputWriterStat.flush();
-			outputWriterStat.close();
-		}
+		StudentService.studentWriter(studentArray);
 	}
-	}
+
+}
+
--- src/com/coderscampus/main/StudentService.java
+++ src/com/coderscampus/main/StudentService.java
@@ -1,5 +1,11 @@
 package com.coderscampus.main;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Comparator;
+
 public class StudentService {
 	public StudentInfo createStudent(int studentID, String studentName, String course, int grade) {
 		StudentInfo studentInfo = new StudentInfo();
@@ -8,5 +14,61 @@ public class StudentService {
 		studentInfo.setCourse(course);
 		studentInfo.setGrade(grade);
 		return studentInfo;
+	}
+	public static void studentWriter(StudentInfo[] studentArray) throws IOException {
+		try (BufferedWriter outputWriterCompsci = new BufferedWriter(new FileWriter("Course1.csv"));
+				BufferedWriter outputWriterApmth = new BufferedWriter(new FileWriter("Course2.csv"));
+				BufferedWriter outputWriterStat = new BufferedWriter(new FileWriter("Course3.csv"))) {
+
+			Arrays.sort(studentArray, new Comparator<StudentInfo>() {
+
+				@Override
+				public int compare(StudentInfo person2, StudentInfo person1) {
+					return Integer.compare(person1.getGrade(), person2.getGrade());
+				}
+			});
+
+			StudentInfo[] compsciArray = new StudentInfo[34];
+			StudentInfo[] apmthArray = new StudentInfo[33];
+			StudentInfo[] statArray = new StudentInfo[33];
+		
+			int csCtr = 0;
+			int apCtr = 0;
+			int statCtr = 0;
+
+			for (StudentInfo student : studentArray) {
+				if (student.getCourse().contains("COMPSCI")) {
+					compsciArray[csCtr++] = student;
+				}
+				if (student.getCourse().contains("APMTH")) {
+					apmthArray[apCtr++] = student;
+				}
+				if (student.getCourse().contains("STAT")) {
+					statArray[statCtr++] = student;
+				}
+			}
+			// this all can be refactored with removing lines 35-37 and changing my for loop ^^^^
+
+			for (int i = 0; i < compsciArray.length; i++) {
+				outputWriterCompsci.write(compsciArray[i] + "");
+				outputWriterCompsci.newLine();
+			}
+			outputWriterCompsci.flush();
+			outputWriterCompsci.close();
+
+			for (int i = 0; i < apmthArray.length; i++) {
+				outputWriterApmth.write(apmthArray[i] + "");
+				outputWriterApmth.newLine();
+			}
+			outputWriterApmth.flush();
+			outputWriterApmth.close();
+
+			for (int i = 0; i < statArray.length; i++) {
+				outputWriterStat.write(statArray[i] + "");
+				outputWriterStat.newLine();
+			}
+			outputWriterStat.flush();
+			outputWriterStat.close();
+		}
 	}
 }
